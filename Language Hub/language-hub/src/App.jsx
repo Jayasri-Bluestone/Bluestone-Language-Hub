ef7d17c1fe223e9d7f0246cd393b49a5b119f499
Jayasri-Bluestone <bluestonesoftwaredeveloper@gamil.com>
2026-03-24 09:57:39 +0530

    update

--- Language Hub/language-hub/src/App.jsx
+++ Language Hub/language-hub/src/App.jsx
@@ -1,52 +1,64 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
 import ContactModal from './components/ContactModal';
-import Home from './pages/Home';
+
+// Loading fallback component
+const PageLoader = () => (
+  <div className="min-h-[60vh] flex items-center justify-center">
+    <div className="flex flex-col items-center gap-4">
+      <div className="w-12 h-12 border-4 border-brand-green/20 border-t-brand-green rounded-full animate-spin"></div>
+      <p className="text-gray-500 font-medium animate-pulse">Loading experience...</p>
+    </div>
+  </div>
+);
+
+// Home
+const Home = lazy(() => import('./pages/Home'));
 
 // Tests
-import IELTS from './pages/tests/IELTS';
-import PTE from './pages/tests/PTE';
-import OET from './pages/tests/OET';
-import DuolingoPage from './pages/tests/Duolingo';
-import TOEFLPage from './pages/tests/TOEFL';
+const IELTS = lazy(() => import('./pages/tests/IELTS'));
+const PTE = lazy(() => import('./pages/tests/PTE'));
+const OET = lazy(() => import('./pages/tests/OET'));
+const DuolingoPage = lazy(() => import('./pages/tests/Duolingo'));
+const TOEFLPage = lazy(() => import('./pages/tests/TOEFL'));
 
 // Abroad
-import CanadaPage from './pages/abroad/Canada';
-import USAPage from './pages/abroad/USA';
-import UKPage from './pages/abroad/UK';
-import AustraliaPage from './pages/abroad/Australia';
-import NewZealandPage from './pages/abroad/NewZealand';
-import IrelandPage from './pages/abroad/Ireland';
-import GermanyAbroad from './pages/abroad/Germany';
-import FranceAbroad from './pages/abroad/France';
-import SingaporePage from './pages/abroad/Singapore';
-import ItalyPage from './pages/abroad/Italy';
-import MalaysiaPage from './pages/abroad/Malaysia';
-import SwedenPage from './pages/abroad/Sweden';
-import NetherlandsPage from './pages/abroad/Netherlands';
-import DenmarkPage from './pages/abroad/Denmark';
-import BulgariaPage from './pages/abroad/Bulgaria';
-import RussiaPage from './pages/abroad/Russia';
-import SwitzerlandPage from './pages/abroad/Switzerland';
-import SouthKoreaPage from './pages/abroad/SouthKorea';
-import UAEPage from './pages/abroad/UAE';
-import PhilippinesPage from './pages/abroad/Philippines';
+const CanadaPage = lazy(() => import('./pages/abroad/Canada'));
+const USAPage = lazy(() => import('./pages/abroad/USA'));
+const UKPage = lazy(() => import('./pages/abroad/UK'));
+const AustraliaPage = lazy(() => import('./pages/abroad/Australia'));
+const NewZealandPage = lazy(() => import('./pages/abroad/NewZealand'));
+const IrelandPage = lazy(() => import('./pages/abroad/Ireland'));
+const GermanyAbroad = lazy(() => import('./pages/abroad/Germany'));
+const FranceAbroad = lazy(() => import('./pages/abroad/France'));
+const SingaporePage = lazy(() => import('./pages/abroad/Singapore'));
+const ItalyPage = lazy(() => import('./pages/abroad/Italy'));
+const MalaysiaPage = lazy(() => import('./pages/abroad/Malaysia'));
+const SwedenPage = lazy(() => import('./pages/abroad/Sweden'));
+const NetherlandsPage = lazy(() => import('./pages/abroad/Netherlands'));
+const DenmarkPage = lazy(() => import('./pages/abroad/Denmark'));
+const BulgariaPage = lazy(() => import('./pages/abroad/Bulgaria'));
+const RussiaPage = lazy(() => import('./pages/abroad/Russia'));
+const SwitzerlandPage = lazy(() => import('./pages/abroad/Switzerland'));
+const SouthKoreaPage = lazy(() => import('./pages/abroad/SouthKorea'));
+const UAEPage = lazy(() => import('./pages/abroad/UAE'));
+const PhilippinesPage = lazy(() => import('./pages/abroad/Philippines'));
 
 // Company
-import AboutUsPage from './pages/company/AboutUsPage';
-import SuccessStories from './pages/company/SuccessStories';
-import Blog from './pages/company/Blog';
-import Gallery from './pages/company/Gallery';
-import FAQPage from './pages/company/FAQPage';
+const AboutUsPage = lazy(() => import('./pages/company/AboutUsPage'));
+const SuccessStories = lazy(() => import('./pages/company/SuccessStories'));
+const Blog = lazy(() => import('./pages/company/Blog'));
+const Gallery = lazy(() => import('./pages/company/Gallery'));
+const FAQPage = lazy(() => import('./pages/company/FAQPage'));
  
 // Courses
-import GermanPage from './pages/courses/German';
-import JapanesePage from './pages/courses/Japanese';
-import FrenchPage from './pages/courses/French';
-import Contact from './components/Contact';
+const GermanPage = lazy(() => import('./pages/courses/German'));
+const JapanesePage = lazy(() => import('./pages/courses/Japanese'));
+const FrenchPage = lazy(() => import('./pages/courses/French'));
+const Contact = lazy(() => import('./components/Contact'));
 
 
 function App() {
@@ -56,60 +68,63 @@ function App() {
       <ContactModal />
       <div className="relative min-h-screen bg-white font-sans text-gray-900">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          
-          {/* Tests */}
-          <Route path="/tests/ielts" element={<IELTS />} />
-          <Route path="/tests/pte" element={<PTE />} />
-          <Route path="/tests/oet" element={<OET />} />
-          <Route path="/tests/duolingo" element={<DuolingoPage />} />
-          <Route path="/tests/toefl" element={<TOEFLPage />} />
+        <Suspense fallback={<PageLoader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            
+            {/* Tests */}
+            <Route path="/tests/ielts" element={<IELTS />} />
+            <Route path="/tests/pte" element={<PTE />} />
+            <Route path="/tests/oet" element={<OET />} />
+            <Route path="/tests/duolingo" element={<DuolingoPage />} />
+            <Route path="/tests/toefl" element={<TOEFLPage />} />
 
-          {/* Abroad */}
-          <Route path="/abroad/canada" element={<CanadaPage />} />
-          <Route path="/abroad/usa" element={<USAPage />} />
-          <Route path="/abroad/uk" element={<UKPage />} />
-          <Route path="/abroad/australia" element={<AustraliaPage />} />
-          <Route path="/abroad/new-zealand" element={<NewZealandPage />} />
-          <Route path="/abroad/ireland" element={<IrelandPage />} />
-          <Route path="/abroad/germany" element={<GermanyAbroad />} />
-          <Route path="/abroad/france" element={<FranceAbroad />} />
-          <Route path="/abroad/singapore" element={<SingaporePage />} />
-          <Route path="/abroad/italy" element={<ItalyPage />} />
-          <Route path="/abroad/malaysia" element={<MalaysiaPage />} />
-          <Route path="/abroad/sweden" element={<SwedenPage />} />
-          <Route path="/abroad/netherlands" element={<NetherlandsPage />} />
-          <Route path="/abroad/denmark" element={<DenmarkPage />} />
-          <Route path="/abroad/bulgaria" element={<BulgariaPage />} />
-          <Route path="/abroad/russia" element={<RussiaPage />} />
-          <Route path="/abroad/switzerland" element={<SwitzerlandPage />} />
-          <Route path="/abroad/south-korea" element={<SouthKoreaPage />} />
-          <Route path="/abroad/uae" element={<UAEPage />} />
-          <Route path="/abroad/philippines" element={<PhilippinesPage />} />
+            {/* Abroad */}
+            <Route path="/abroad/canada" element={<CanadaPage />} />
+            <Route path="/abroad/usa" element={<USAPage />} />
+            <Route path="/abroad/uk" element={<UKPage />} />
+            <Route path="/abroad/australia" element={<AustraliaPage />} />
+            <Route path="/abroad/new-zealand" element={<NewZealandPage />} />
+            <Route path="/abroad/ireland" element={<IrelandPage />} />
+            <Route path="/abroad/germany" element={<GermanyAbroad />} />
+            <Route path="/abroad/france" element={<FranceAbroad />} />
+            <Route path="/abroad/singapore" element={<SingaporePage />} />
+            <Route path="/abroad/italy" element={<ItalyPage />} />
+            <Route path="/abroad/malaysia" element={<MalaysiaPage />} />
+            <Route path="/abroad/sweden" element={<SwedenPage />} />
+            <Route path="/abroad/netherlands" element={<NetherlandsPage />} />
+            <Route path="/abroad/denmark" element={<DenmarkPage />} />
+            <Route path="/abroad/bulgaria" element={<BulgariaPage />} />
+            <Route path="/abroad/russia" element={<RussiaPage />} />
+            <Route path="/abroad/switzerland" element={<SwitzerlandPage />} />
+            <Route path="/abroad/south-korea" element={<SouthKoreaPage />} />
+            <Route path="/abroad/uae" element={<UAEPage />} />
+            <Route path="/abroad/philippines" element={<PhilippinesPage />} />
 
-          {/* Company */}
-          <Route path="/company/about" element={<AboutUsPage />} />
-          <Route path="/company/success" element={<SuccessStories />} />
-          <Route path="/company/blog" element={<Blog />} />
-          <Route path="/company/gallery" element={<Gallery />} />
-          <Route path="/company/faq" element={<FAQPage />} />
- 
-          {/* Courses */}
-          <Route path="/courses/german" element={<GermanPage />} />
-          <Route path="/courses/japanese" element={<JapanesePage />} />
-          <Route path="/courses/french" element={<FrenchPage />} />
+            {/* Company */}
+            <Route path="/company/about" element={<AboutUsPage />} />
+            <Route path="/company/success" element={<SuccessStories />} />
+            <Route path="/company/blog" element={<Blog />} />
+            <Route path="/company/gallery" element={<Gallery />} />
+            <Route path="/company/faq" element={<FAQPage />} />
+   
+            {/* Courses */}
+            <Route path="/courses/german" element={<GermanPage />} />
+            <Route path="/courses/japanese" element={<JapanesePage />} />
+            <Route path="/courses/french" element={<FrenchPage />} />
 
-          {/* Contact */}
-          <Route path='/contact' element={<Contact />} />
-          
+            {/* Contact */}
+            <Route path='/contact' element={<Contact />} />
+            
 
-        </Routes>
+          </Routes>
+        </Suspense>
         <Footer />
       </div>
     </Router>
   );
 }
+
 
 export default App;
 
